12b0edb1074146bfc80cafe558f8802b8964bfb8
alancodvo <alancodvo@gmail.com>
2014-12-16 16:04:30 +0900

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by alan-arakawa-yoshihiro on H26/12/16.
+//  Copyright (c) 平成26年 Yoshihiro Arakawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
